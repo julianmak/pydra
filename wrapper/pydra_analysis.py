d45df3004bd879e475e89fef461ad889bb5110e8
Julian Mak <julian.c.l.mak@googlemail.com>
2018-10-29 12:29:35 +0000

    moved some default routines around, added GEOMETRIC notebook

--- wrapper/pydra_analysis.py
+++ wrapper/pydra_analysis.py
@@ -1,182 +1,112 @@
 #!/usr/bin/env python3
 #
-# JM, 20 Apr 2018
+# JM, 29 Oct 2018
 #
 # some subfunctions to do analysis of pydra data
 
-from numpy import zeros, mean
-from numpy.fft import rfft, irfft
+from pydra_misc import * # numpy is loaded here as np
+from casl import parameters, spectral, constants
 
 #-------------------------------------------------------------------------------
-def zonal_ave(infield):
-  """
-  Subfunction to zonally average some input 2d field. Assumes it is uniformly 
-  spaced in x (axis = 1)
-  
-  Input:
-    infield     2d input field for data
-  
-  Output:
-    outfield    1d zonally averaged data
-  """
-  
-  outfield = mean(infield, axis = 1)
-  
-  return outfield
-  
-#-------------------------------------------------------------------------------
-def zonal_demean(infield):
-  """
-  Subfunction to get rid of the zonal mean of the input field
-  
-  Input:
-    infield     2d input field for data
-  
-  Output:
-    outfield    2d output field for data
-  """
-  
-  mean_1d = zonal_ave(infield)
-  field_mean = zeros(infield.shape)
-  
-  for i in range(infield.shape[1]):
-    field_mean[:, i] = mean_1d # probably a faster way of doing this (broadcast?)
-  
-  outfield = infield - field_mean
-  
-  return outfield
+# generate the EKE in layers and modes
 
-#-------------------------------------------------------------------------------
-def zonal_corr(f1_in, f2_in):
-  """
-  Subfunction to generate the correlation function of two input fields,
-  as defined by the zonal average
-  
-  e.g. u_in and v_in would give Reynolds stress
-       u_in and u_in would give part of M in geometric decomposition
-  
-  can do (u_in, u_in) and (v_in, v_in) to give eke
-  
-  Input:
-    f1_in, f2_in 2d input field
-  
-  Output:
-    f1f2_out     2d output demean-ed field
-  """
-  
-  # do a de-mean of both fields even if some of them have no mean 
-  # (e.g. v = d(psi)/dx)
-  f1_fluc = zonal_demean(f1_in)
-  f2_fluc = zonal_demean(f2_in)
-  
-  f1f2_out = f1_fluc * f2_fluc
-  
-  return f1f2_out
-  
-#-------------------------------------------------------------------------------
-def zonal_eke(u_in, v_in):
-  """
-  Subfunction to generate the eke field as defined by the zonal average
-  
-  Input:
-    u_in, v_in  2d input velocity field
-  
-  Output:
-    eke         2d output eke field
-  """
-  
-  eke = (zonal_corr(u_in, u_in) + zonal_corr(v_in, v_in)) / 2.0
-  
-  return eke
-  
-#-------------------------------------------------------------------------------
-def zonal_eke_int(u1_in, v1_in, u2_in, v2_in, parameters):
-  """
-  Subfunction to compute the domain-averaged eke value, where the
-  eke is defined by the zonal average (give it the layer velocity though)
-  
-  Input:
-    u_in, v_in  2d layer input velocity field
-    parameters  collection of system parameters
-  
-  Output:
-    eke_domavg  domain integrated value
-    (TODO: eke_max?)
-  """
-  # work out the eke of layers then weight it with layer depth (which is 1)
-  eke1 = zonal_eke(u1_in, v1_in) * parameters.h1
-  eke2 = zonal_eke(u2_in, v2_in) * (1 - parameters.h1)
-  
-  # add them together and take a mean (divide by depth = 1 not written in)
-  eke_domavg = mean(mean(eke1 + eke2, axis = 0), axis = 0)
-  
-  return eke_domavg
-  
-#-------------------------------------------------------------------------------
-def zonal_ens(q_in):
-  """
-  Subfunction to generate the enstrophy field as defined by the zonal average
-  
-  Input:
-    q_in        2d input PV field
-  
-  Output:
-    ens         2d output enstrophy field
-  """
-  
-  ens = zonal_corr(q_in, q_in) / 2.0
-  
-  return ens
+def calc_eke(data_dir, parameters, constants, kt):
 
-#-------------------------------------------------------------------------------
-def zonal_ens_int(q1_in, q2_in, parameters):
   """
-  Subfunction to compute the domain-averaged ens value, where the
-  ens is defined by the zonal average (give it the layer PV though)
+  Subfunction to generate EKE in layers and modes (using zonal mean)
   
   Input:
-    q_in        2d layer input PV field
-    parameters  collection of system parameters
+    data_dir    data directory
+    parameters  parameter module from load
+    constants   constants module from load
+    kt          time stamp
   
   Output:
-    ens_domavg  domain integrated value
-    (TODO: ens_max?)
+    eke_L1L2    2d field of EKE in layers
+    eke_btbc    2d field of EKE in modes
   """
-  # work out the eke of layers then weight it with layer depth (which is 1)
-  ens1 = zonal_ens(q1_in) * parameters.h1
-  ens2 = zonal_ens(q2_in) * (1 - parameters.h1)
   
-  # add them together and take a mean (divide by depth = 1 not written in)
-  ens_domavg = mean(mean(ens1 + ens2, axis = 0), axis = 0)
+  t_now, qq = read_qq(data_dir, parameters.nx, parameters.ny, kt)
+  # swap axis to have the indexing consistent with the Fortran code main_invert
+  qq = np.swapaxes(qq, 0, 1)
+
+  # no topography
+  fhb = np.zeros((parameters.ny + 1, parameters.nx))
+
+  # invert for the velocity fields
+  uu, vv, _ = spectral.main_invert(qq, fhb)
   
-  return ens_domavg
+  uu_btbc = layers_to_modes(uu, constants)
+  vv_btbc = layers_to_modes(vv, constants)
+  
+  # calculate the EKE
+  eke_L1L2 = np.zeros(uu.shape)
+  eke_btbc = np.zeros(uu_btbc.shape)
+  eke_L1L2[:, :, 0] = zonal_eke(uu[:, :, 0], vv[:, :, 0])
+  eke_L1L2[:, :, 1] = zonal_eke(uu[:, :, 1], vv[:, :, 1])
+  eke_btbc[:, :, 0] = zonal_eke(uu_btbc[:, :, 0], vv_btbc[:, :, 0])
+  eke_btbc[:, :, 1] = zonal_eke(uu_btbc[:, :, 1], vv_btbc[:, :, 1])
+
+  return (eke_L1L2, eke_btbc)
   
 #-------------------------------------------------------------------------------
-def zonal_mode_extract(infield, mode_keep, low_pass = False):
+# generate the M and N
+
+def calc_geom_momentum(data_dir, parameters, constants, kt):
+
   """
-  Subfunction to extract or swipe out zonal modes (mode_keep) of (y, x) data.
-  Assumes here that the data is periodic in axis = 1 (in the x-direction) with
-  the end point missing
-  
-  If mode_keep = 0 then this is just the zonal averaged field
+  Subfunction to generate M = <v'v' - u'u'> / 2 and N = <u'v'> 
+  (using zonal mean)
   
   Input:
-    in_field    2d layer input field
-    mode_keep   the zonal mode of the data to be extracted from
-    
-  Opt input:
-    low_pass    get rid of all modes from mode_keep + 1 onwards
+    data_dir    data directory
+    parameters  parameter module from load
+    constants   constants module from load
+    kt          time stamp
   
   Output:
-    outfield    zonal mode of the data
+    M/N_L1L2    2d field of M/N in layers
+    M/N_btbc    2d field of M/N in modes
   """
   
-  outfield_h = rfft(infield, axis = 1)
-  outfield_h[:, mode_keep+1::] = 0
-  if not low_pass:
-    outfield_h[:, 0:mode_keep] = 0
+  t_now, qq = read_qq(data_dir, parameters.nx, parameters.ny, kt)
+  # swap axis to have the indexing consistent with the Fortran code main_invert
+  qq = np.swapaxes(qq, 0, 1)
+
+  # no topography
+  fhb = np.zeros((parameters.ny + 1, parameters.nx))
+
+  # invert for the velocity fields
+  uu, vv, _ = spectral.main_invert(qq, fhb)
   
-  return irfft(outfield_h, axis = 1)
+  uu_btbc = layers_to_modes(uu, constants)
+  vv_btbc = layers_to_modes(vv, constants)
+  
+  # calculate the quantities
+  M_L1L2 = np.zeros(uu.shape)
+  N_L1L2 = np.zeros(uu.shape)
+  M_btbc = np.zeros(uu_btbc.shape)
+  N_btbc = np.zeros(uu_btbc.shape)
+  
+  zonal_demean(vv[:, :, 0])
+  
+  M_L1L2[:, :, 0] = (zonal_corr(vv[:, :, 0], vv[:, :, 0])
+                   - zonal_corr(uu[:, :, 0], uu[:, :, 0]))
+  M_L1L2[:, :, 1] = (zonal_corr(vv[:, :, 1], vv[:, :, 1])
+                   - zonal_corr(uu[:, :, 1], uu[:, :, 1]))
+  N_L1L2[:, :, 0] =  zonal_corr(vv[:, :, 0], uu[:, :, 0])
+  N_L1L2[:, :, 1] =  zonal_corr(vv[:, :, 1], uu[:, :, 1])
+  
+  M_btbc[:, :, 0] = (zonal_corr(vv_btbc[:, :, 0], vv_btbc[:, :, 0])
+                   - zonal_corr(uu_btbc[:, :, 0], uu_btbc[:, :, 0]))
+  M_btbc[:, :, 1] = (zonal_corr(vv_btbc[:, :, 1], vv_btbc[:, :, 1])
+                   - zonal_corr(uu_btbc[:, :, 1], uu_btbc[:, :, 1]))
+  N_btbc[:, :, 0] =  zonal_corr(vv_btbc[:, :, 0], uu_btbc[:, :, 0])
+  N_btbc[:, :, 1] =  zonal_corr(vv_btbc[:, :, 1], uu_btbc[:, :, 1])
+
+  return (M_L1L2, N_L1L2, M_btbc, N_btbc)
+
+
 
 
 
--- wrapper/pydra_misc.py
+++ wrapper/pydra_misc.py
@@ -5,7 +5,8 @@
 # some misc functions relating to pydra
 
 import os
-from numpy import fromfile, float32, delete, zeros
+import numpy as np
+from numpy.fft import rfft, irfft
 
 #-------------------------------------------------------------------------------
 # read the PV data
@@ -27,7 +28,7 @@ def read_qq(data_dir, nx, ny, kt, num_frame = False):
   
   N = nx * (ny + 1) + 1
   
-  qq = zeros((nx, ny + 1, 2))
+  qq = np.zeros((nx, ny + 1, 2))
 
   qq1_filename = data_dir + "qq1.r4"
 
@@ -38,7 +39,7 @@ def read_qq(data_dir, nx, ny, kt, num_frame = False):
   if num_frame:
     print("number of frames found = %i " % nframes)
 
-  raw_array = fromfile(qq1_file, dtype = float32)
+  raw_array = np.fromfile(qq1_file, dtype = np.float32)
   time_eles = [i * N for i in range(nframes)]
 
   # pull out the header time
@@ -55,7 +56,7 @@ def read_qq(data_dir, nx, ny, kt, num_frame = False):
   
   qq2_filename = data_dir + "qq2.r4"
   qq2_file = open(qq2_filename, "r")
-  raw_array = fromfile(qq2_file, dtype = float32)
+  raw_array = np.fromfile(qq2_file, dtype = np.float32)
   qq[:, :, 1] = raw_array[index_start:index_end].reshape((nx, ny + 1))
   
   qq2_file.close()
@@ -65,21 +66,192 @@ def read_qq(data_dir, nx, ny, kt, num_frame = False):
 #-------------------------------------------------------------------------------
 # transform between layer fields to modal fields
 
-def layers_to_modes(field_top, field_low, constants):
+def layers_to_modes(field_L1L2, constants):
 
   """
   Subfunction to transform layer fields to modal fields
   
   Input:
-    field_top   2d field of top layer
-    field_low   2d field of lower layer
+    field_L1L2 2d field in layers
   
   Output:
-    field_bt    2d field of barotropic mode
-    field_bc    2d field of baroclinic mode
+    field_btbc 2d field in modes
   """
   
-  field_bt = constants.vec11 * field_top + constants.vec12 * field_low
-  field_bc = constants.vec21 * field_top + constants.vec22 * field_low
+  field_btbc = np.zeros(field_L1L2.shape)
   
-  return (field_bt, field_bc)
+  field_btbc[:, :, 0] = (constants.vec11 * field_L1L2[:, :, 0] 
+                       + constants.vec12 * field_L1L2[:, :, 1])
+  field_btbc[:, :, 1] = (constants.vec21 * field_L1L2[:, :, 0] 
+                       + constants.vec22 * field_L1L2[:, :, 1])
+
+  return field_btbc
+  
+#-------------------------------------------------------------------------------
+def zonal_ave(infield):
+  """
+  Subfunction to zonally average some input 2d field. Assumes it is uniformly 
+  spaced in x (axis = 1)
+  
+  Input:
+    infield     2d input field for data
+  
+  Output:
+    outfield    1d zonally averaged data
+  """
+  
+  outfield = np.mean(infield, axis = 1)
+  
+  return outfield
+  
+#-------------------------------------------------------------------------------
+def zonal_demean(infield):
+  """
+  Subfunction to get rid of the zonal mean of the input field
+  
+  Input:
+    infield     2d input field for data
+  
+  Output:
+    outfield    2d output field for data
+  """
+  
+  mean_1d = zonal_ave(infield)
+  outfield = np.zeros(infield.shape)
+  
+  for i in range(infield.shape[1]):
+    outfield[:, i] = infield[:, i] - mean_1d # probably a faster way of doing this (broadcast?)
+  
+  return outfield
+
+#-------------------------------------------------------------------------------
+def zonal_corr(f1_in, f2_in):
+  """
+  Subfunction to generate the correlation function of two input fields,
+  as defined by the zonal average
+  
+  e.g. u_in and v_in would give Reynolds stress
+       u_in and u_in would give part of M in geometric decomposition
+  
+  can do (u_in, u_in) and (v_in, v_in) to give eke
+  
+  Input:
+    f1_in, f2_in 2d input field
+  
+  Output:
+    f1f2_out     2d output demean-ed field
+  """
+  
+  # do a de-mean of both fields even if some of them have no mean 
+  # (e.g. v = d(psi)/dx)
+  f1_fluc = zonal_demean(f1_in)
+  f2_fluc = zonal_demean(f2_in)
+  
+  f1f2_out = f1_fluc * f2_fluc
+  
+  return f1f2_out
+  
+#-------------------------------------------------------------------------------
+def zonal_eke(u_in, v_in):
+  """
+  Subfunction to generate the eke field as defined by the zonal average
+  
+  Input:
+    u_in, v_in  2d input velocity field
+  
+  Output:
+    eke         2d output eke field
+  """
+  
+  eke = (zonal_corr(u_in, u_in) + zonal_corr(v_in, v_in)) / 2.0
+  
+  return eke
+  
+#-------------------------------------------------------------------------------
+def zonal_eke_int(u1_in, v1_in, u2_in, v2_in, parameters):
+  """
+  Subfunction to compute the domain-averaged eke value, where the
+  eke is defined by the zonal average (give it the layer velocity though)
+  
+  Input:
+    u_in, v_in  2d layer input velocity field
+    parameters  collection of system parameters
+  
+  Output:
+    eke_domavg  domain integrated value
+    (TODO: eke_max?)
+  """
+  # work out the eke of layers then weight it with layer depth (which is 1)
+  eke1 = zonal_eke(u1_in, v1_in) * parameters.h1
+  eke2 = zonal_eke(u2_in, v2_in) * (1 - parameters.h1)
+  
+  # add them together and take a mean (divide by depth = 1 not written in)
+  eke_domavg = np.mean(np.mean(eke1 + eke2, axis = 0), axis = 0)
+  
+  return eke_domavg
+  
+#-------------------------------------------------------------------------------
+def zonal_ens(q_in):
+  """
+  Subfunction to generate the enstrophy field as defined by the zonal average
+  
+  Input:
+    q_in        2d input PV field
+  
+  Output:
+    ens         2d output enstrophy field
+  """
+  
+  ens = zonal_corr(q_in, q_in) / 2.0
+  
+  return ens
+
+#-------------------------------------------------------------------------------
+def zonal_ens_int(q1_in, q2_in, parameters):
+  """
+  Subfunction to compute the domain-averaged ens value, where the
+  ens is defined by the zonal average (give it the layer PV though)
+  
+  Input:
+    q_in        2d layer input PV field
+    parameters  collection of system parameters
+  
+  Output:
+    ens_domavg  domain integrated value
+    (TODO: ens_max?)
+  """
+  # work out the eke of layers then weight it with layer depth (which is 1)
+  ens1 = zonal_ens(q1_in) * parameters.h1
+  ens2 = zonal_ens(q2_in) * (1 - parameters.h1)
+  
+  # add them together and take a mean (divide by depth = 1 not written in)
+  ens_domavg = np.mean(np.mean(ens1 + ens2, axis = 0), axis = 0)
+  
+  return ens_domavg
+  
+#-------------------------------------------------------------------------------
+def zonal_mode_extract(infield, mode_keep, low_pass = False):
+  """
+  Subfunction to extract or swipe out zonal modes (mode_keep) of (y, x) data.
+  Assumes here that the data is periodic in axis = 1 (in the x-direction) with
+  the end point missing
+  
+  If mode_keep = 0 then this is just the zonal averaged field
+  
+  Input:
+    in_field    2d layer input field
+    mode_keep   the zonal mode of the data to be extracted from
+    
+  Opt input:
+    low_pass    get rid of all modes from mode_keep + 1 onwards
+  
+  Output:
+    outfield    zonal mode of the data
+  """
+  
+  outfield_h = rfft(infield, axis = 1)
+  outfield_h[:, mode_keep+1::] = 0
+  if not low_pass:
+    outfield_h[:, 0:mode_keep] = 0
+  
+  return irfft(outfield_h, axis = 1)
